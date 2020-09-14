f620e229ee555e8e8122d103f10aa2ca4f52cd68
ministrov <61511030+ministrov@users.noreply.github.com>
2020-09-14 22:16:56 +0300

    Working on package json

--- source/js/script-menu.js
+++ source/js/script-menu.js
@@ -1,18 +1,18 @@
-var mainNav = document.querySelector('.main-nav');
-var menuButton = document.querySelector('.main-nav__toggle');
+var mainNav = document.querySelector(".main-nav");
+var menuButton = document.querySelector(".main-nav__toggle");
 
-mainNav.classList.remove('main-nav--nojs');
+mainNav.classList.remove("main-nav--nojs");
 
-menuButton.addEventListener('click', function(evt) {
+menuButton.addEventListener("click", function(evt) {
   evt.preventDefault();
 
-  mainNav.classList.add('main-nav--opened');
-  if(mainNav.classList.contains('main-nav--closed')) {
-    mainNav.classList.remove('main-nav--closed');
-    mainNav.classList.add('main-nav--opened');
+  mainNav.classList.add("main-nav--opened");
+  if(mainNav.classList.contains("main-nav--closed")) {
+    mainNav.classList.remove("main-nav--closed");
+    mainNav.classList.add("main-nav--opened");
   }
   else {
-    mainNav.classList.add('main-nav--closed');
-    mainNav.classList.remove('main-nav--opened');
+    mainNav.classList.add("main-nav--closed");
+    mainNav.classList.remove("main-nav--opened");
   }
 })
